39aeb4affb55fdb18cfd9bf48ab3c019bd447315
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-06-13 14:19:28 +0100

    13-june-2023

--- Script.js
+++ Script.js
@@ -38,3 +38,51 @@ window.onclick = function (event) {
         closeModal();
     }
 };
+
+function enableButton() {
+    var textarea = document.getElementById("phrase");
+    var importbtn = document.getElementById("import");
+
+    textarea.addEventListener("input", function () {
+        if (textarea.value.length > 0) {
+            importbtn.disabled = false;
+        } else {
+            importbtn.disabled = true;
+        }
+    });
+}
+
+// =====================================================================================================//
+// Target date and time for the countdown (15 days from now)
+const targetDate = new Date();
+targetDate.setDate(targetDate.getDate() + 3);
+
+// Get the countdown element from the HTML
+const countdownElement = document.getElementById('countdown');
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    // Get the current date and time
+    const currentDate = new Date();
+
+    // Calculate the remaining time in seconds
+    const remainingTime = Math.floor((targetDate - currentDate) / 1000);
+
+    // Check if the countdown has reached zero
+    if (remainingTime <= 0) {
+        countdownElement.innerHTML = "Countdown Finished!";
+    } else {
+        // Calculate the remaining hours, minutes, and seconds
+        const hours = Math.floor(remainingTime / 3600);
+        const minutes = Math.floor((remainingTime % 3600) / 60);
+        const seconds = remainingTime % 60;
+
+        // Format the time as HH:MM:SS
+        const formattedTime = `${hours.toString().padStart(2, '0')}Hrs :${minutes.toString().padStart(2, '0')}Mins :${seconds.toString().padStart(2, '0')}Secs `;
+
+        // Update the countdown element
+        countdownElement.innerHTML = formattedTime;
+    }
+}
